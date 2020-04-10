81dbd56e3e5c1444766a7955b7161477ceeea28d
Harsh Kapadia <harshgkapadia@gmail.com>
2020-04-10 13:04:15 +0530

    Modify base.html & add firebaseConfig.js
    Remove unnecessary scripts, add firebaseConfig.js to replace with external script tag & add async & defer tags to base.html

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
