d685e28e4ee0cc28721843f19337a67bca2ae5b6
SaikiranJakkan <saikiran.jakkan@gmail.com>
2020-06-26 22:10:40 +0530

    :sparkles: feat: Create `add-edit-lecture.html` and modify `teacher.py`

--- teacher/teacher.py
+++ teacher/teacher.py
@@ -29,3 +29,23 @@ teacher = Blueprint('teacher', __name__, static_folder='static', template_folder
 def classDashboard():
   if request.method == 'GET':
     return render_template('class-dashboard.html')
+
+@teacher.route('/division_dashboard', methods=['GET'])
+def divisionDashboard():
+  if request.method == 'GET':
+    return render_template('division-dashboard.html')
+
+@teacher.route('/defaulter_calculation', methods=['GET'])
+def defaulterCalculation():
+  if request.method == 'GET':
+    return render_template('defaulter-calculation.html')
+
+@teacher.route('/defaulter_list', methods=['GET'])
+def defaulterList():
+  if request.method == 'GET':
+    return render_template('defaulter-list.html')
+
+@teacher.route('/add_edit_lecture', methods=['GET'])
+def add_editLecture():
+  if request.method == 'GET':
+    return render_template('add-edit-lecture.html')
